6eac48e8a27ab4488a80cbc29bdaea577309d0cc
sullof <sullof@gmail.com>
2018-01-27 18:10:21 -0800

    Fully decentralized version

--- api/docker/node-debug.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -it --rm \
-  --name tweedentity-debug \
-  -p 3132 \
-  -v $PWD:/usr/src/app \
-  -v $PWD/log:/var/log/tweedentity \
-  -w /usr/src/app node:carbon npm run start
