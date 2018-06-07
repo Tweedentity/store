6c836c11c8891f8ebdb7d599503246b063325b35
sullof <sullof@gmail.com>
2018-06-06 19:16:18 -0700

    Minor fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ var TweedentityManager = artifacts.require("./TweedentityManager")
 var TweedentityClaimer = artifacts.require("./TweedentityClaimer")
 
 module.exports = function(deployer) {
-  // deployer.deploy(TweedentityStore)
-  // deployer.deploy(TweedentityManager)
+  deployer.deploy(TweedentityStore)
+  deployer.deploy(TweedentityManager)
   deployer.deploy(TweedentityClaimer)
 }
